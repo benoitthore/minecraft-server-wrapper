34471ec97682ea46b444a00e2d93f4009c3cad66
Benoit Thore <thorebenoit@gmail.com>
2024-11-08 23:27:36 +0000

    .

--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -1,13 +1,17 @@
 package com.example.plugins
 
-import com.example.controllers.MinecraftProcessInstance
+import com.example.app.MinecraftProcess
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import org.koin.ktor.ext.inject
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureSockets() {
+
+    val minecraftProcessInstance by inject<MinecraftProcess>()
+
     install(WebSockets) {
         pingPeriod = 15.seconds
         timeout = 15.seconds
@@ -16,20 +20,20 @@ fun Application.configureSockets() {
     }
     routing {
         webSocket("/ws") {
-            MinecraftProcessInstance.eventFlow.collect { event ->
+            minecraftProcessInstance.eventFlow.collect { event ->
                 outgoing.send(Frame.Text(event.toString()))
             }
         }
-        webSocket("/NOT_USED") { // websocketSession
-            for (frame in incoming) {
-                if (frame is Frame.Text) {
-                    val text = frame.readText()
-                    outgoing.send(Frame.Text("YOU SAID: $text"))
-                    if (text.equals("bye", ignoreCase = true)) {
-                        close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
-                    }
-                }
-            }
-        }
+//        webSocket("/ws") { // websocketSession
+//            for (frame in incoming) {
+//                if (frame is Frame.Text) {
+//                    val text = frame.readText()
+//                    outgoing.send(Frame.Text("YOU SAID: $text"))
+//                    if (text.equals("bye", ignoreCase = true)) {
+//                        close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+//                    }
+//                }
+//            }
+//        }
     }
 }
